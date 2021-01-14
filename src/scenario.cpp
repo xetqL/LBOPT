8720861448e3afcc0b38ebc23297b4f021752091
anthony <boulmieran@gmail.com>
2021-01-14 23:54:33 +0100

    add bastien's technique

--- src/scenario.cpp
+++ src/scenario.cpp
@@ -124,6 +124,57 @@ std::tuple<double, std::vector<bool>, std::vector<double>> create_scenario_menon
 
     return {Tcpu, scenario, imb_time};
 }
+std::tuple<double, std::vector<bool>, std::vector<double>> create_scenario_bastien(SimParam p){
+    std::vector<bool> scenario(p.maxI);
+    std::vector<double> imb_time(p.maxI);
+    std::vector<double> it_max(p.maxI);
+    std::vector<double> it_avg(p.maxI);
+    double U = 0;
+    double P = p.P;
+    double Wmax = p.W0 / p.P;
+    double Wavg = Wmax;
+    double Wmin = Wmax;
+    double Tcpu = 0;
+    double C = p.C;
+
+    Model model = Balanced;
+    State state {model, Wmax, Wavg, Wmin}
+
+    int last_lb_it = 0;
+    double last_Ui = 0;
+    for(int iter = 0; iter < p.maxI; ++iter) {
+        auto&[model, Wmax, Wavg, Wmin] = state;
+        auto tau = iter-last_lb_it;
+        // Based on the previous iteration, should I rebalance ?
+        if ((tau*last_Ui) - U > C) { // trigger load balancing
+            // Reset cumulative LB
+            U = 0;
+            // partitioning and mapping -> load balancing
+            // partitioning and mapping -> load balancing
+            rebalance(state);
+            // Account for the LB cost
+            Tcpu += C;
+            // This iteration is balanced
+            scenario[iter] = true;
+            last_lb_it = iter;
+        }
+
+        // Compute the iteration
+        Tcpu += Wmax;
+        it_max[iter] = Wmax;
+        it_avg[iter] = Wavg;
+        // Measure load imbalance
+        U += compute_U(Wmax, Wavg);
+        last_Ui = compute_U(Wmax, Wavg);
+        // Store cumulative load imbalance
+        imb_time[iter] = U;
+        // Apply the workload increase rate function
+        update_workloads(iter, p.P, p.deltaW, state);
+    }
+    save_results(dir+"menon-solution-1.txt", scenario, p, imb_time, it_max, it_avg);
+    return {Tcpu, scenario, imb_time};
+}
+
 std::tuple<double, std::vector<bool>, std::vector<double>> create_scenario_menon_minus_one(SimParam p){
     std::vector<bool> scenario(p.maxI);
     std::vector<double> imb_time(p.maxI);
